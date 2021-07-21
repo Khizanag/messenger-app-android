302ad6dd682639facb1386f18fc3f118b892cb60
Mariam Ormotsadze <m.ormotsadze@bog-imacs-MacBook-Pro.local>
2021-07-21 22:51:31 +0400

    modify bottomNavigationBar name

--- MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/MainActivity.kt
+++ MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/MainActivity.kt
@@ -6,11 +6,11 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationBar: BottomNavigationView
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottomNavigationBar = findViewById(R.id.bottom_navigation_view)
-        bottomNavigationBar.background = null
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.background = null
     }
 }
