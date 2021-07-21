952d1784f722b11cb577271f090ff273a65a1223
Mariam Ormotsadze <m.ormotsadze@bog-imacs-MacBook-Pro.local>
2021-07-21 22:44:35 +0400

    add bottom navigation menu with floating button

--- MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/MainActivity.kt
+++ MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/MainActivity.kt
@@ -3,10 +3,14 @@ package mariamormotsadze.gigakhizanishvili.messengerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationBar: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationBar = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationBar.background = null
     }
 }
