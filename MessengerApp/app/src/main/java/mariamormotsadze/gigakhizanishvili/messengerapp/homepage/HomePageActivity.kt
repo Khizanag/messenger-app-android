55aa52ebf46470f9e3efed8f4319c2f4b7e83930
Mariam Ormotsadze <m.ormotsadze@bog-imacs-MacBook-Pro.local>
2021-08-06 16:02:20 +0400

    fix little bugs (search box position, sign up button color, navigation bar text)

--- MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/homepage/HomePageActivity.kt
+++ MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/homepage/HomePageActivity.kt
@@ -1,16 +1,20 @@
 package mariamormotsadze.gigakhizanishvili.messengerapp.homepage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuView.ItemView
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import mariamormotsadze.gigakhizanishvili.messengerapp.R
+import mariamormotsadze.gigakhizanishvili.messengerapp.UsersSearchActivity
 import mariamormotsadze.gigakhizanishvili.messengerapp.databinding.ActivityHomePageBinding
 import mariamormotsadze.gigakhizanishvili.messengerapp.fragments.HomeFragment
 import mariamormotsadze.gigakhizanishvili.messengerapp.fragments.SettingsFragment
-import mariamormotsadze.gigakhizanishvili.messengerapp.UsersSearchActivity
 
 class HomePageActivity : AppCompatActivity() {
 //    override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,7 @@ class HomePageActivity : AppCompatActivity() {
         activityHomeBiding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(activityHomeBiding.root)
         initNavigationView()
-
+       // removeTextLabel(bottomNavigationView, (R.id.home_icon))
         val homeFragment = HomeFragment()
         val settingsFragment = SettingsFragment()
 
@@ -57,6 +61,27 @@ class HomePageActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().apply {
             replace(R.id.frame_layout, fragment)
             commit()
+        }
+    }
+
+    private fun removeTextLabel(bottomNavigationView: BottomNavigationView, @IdRes menuItemId: Int) {
+        val view = bottomNavigationView.findViewById<View>(menuItemId) ?: return
+        if (view is ItemView) {
+            val viewGroup = view as ViewGroup
+            var padding = 0
+            for (i in 0 until viewGroup.childCount) {
+                val v = viewGroup.getChildAt(i)
+                if (v is ViewGroup) {
+                    padding = v.getHeight()
+                    viewGroup.removeViewAt(i)
+                }
+            }
+            viewGroup.setPadding(
+                view.getPaddingLeft(),
+                (viewGroup.paddingTop + padding) / 2,
+                view.getPaddingRight(),
+                view.getPaddingBottom()
+            )
         }
     }
 }
