d27da0b259eb679df0d040bc39da288f8415405a
Mariam Ormotsadze <m.ormotsadze@bog-imacs-MacBook-Pro.local>
2021-08-08 00:49:50 +0400

    fix setting page little bugs

--- MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/fragments/SettingsFragment.kt
+++ MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/fragments/SettingsFragment.kt
@@ -1,16 +1,18 @@
 package mariamormotsadze.gigakhizanishvili.messengerapp.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import mariamormotsadze.gigakhizanishvili.messengerapp.R
+import mariamormotsadze.gigakhizanishvili.messengerapp.constants.Constants
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -18,43 +20,31 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var imageView: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_settings, container, false)
+        imageView= view.findViewById<View>(R.id.user_avatar) as ImageView
+        imageView.setOnClickListener(){
+            Log.i("Setting Page", "avatar pressed")
+
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, Constants.PICK_IMAGE)
+
+        }
+
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode == 100){
+           imageView.setImageURI((data!!.data))
+        }
     }
+
 }
--- MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/signin/SignUpActivity.kt
+++ MessengerApp/app/src/main/java/mariamormotsadze/gigakhizanishvili/messengerapp/signin/SignUpActivity.kt
@@ -19,6 +19,7 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
         activitySignUpBinding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(activitySignUpBinding.root)
         setListeners()
